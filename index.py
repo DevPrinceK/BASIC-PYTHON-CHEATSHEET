4e2399687020009fe583c23e953510f5c91cf21d
BraNanaK <princesamuelpks@gmail.com>
2021-08-03 15:16:35 +0000

    Create index.py

---
+++ index.py
@@ -0,0 +1,36 @@
+# PYTHON CHEATSHEET
+
+#0 NOTE :- DATA TYPES  
+
+#1 NOTE :- CREATING VARIABLES
+name = "Prince Kyeremanteng Samuel"     # string variable
+age = 23                                # int variable
+gpa = 3.46                              # floating point variable
+married = False                         # boolean variable
+friends = ["John", "Jane", "Jack"]      # List
+agesOfFriends = {"John":21, "Jane":20, "Jack":22}   # dictionary
+hobbies = {"Coding", "Music", "Movie"}              # set
+ranges = range(1, 20)                               # range
+
+#2 NOTE :- OUTPUTTING VALUES
+# print("Hello world")
+# print(age)
+# print(ranges)
+# print(f"LIST OF MY FRIENDS: {friends}")
+
+#3 NOTE :- INPUTING VALUES
+# newName = input("Enter new name: ")
+# age = input("Enter age: ")
+
+#4 NOTE :- TYPE CASTING
+age = int("24")
+trueOrFalse = bool(0)
+givenBirth = bool(1)
+asciiCode = ord("g")
+setToList = list({1, 2, 3, 4, 5})
+listToSet = set([10, 20, 25, 30, 35])
+nestedListToDic = dict([["name", "Prince"], ["age",23], ["town","Amanfrom"]])
+
+print(nestedListToDic)
+
+#5 NOTE :-  
