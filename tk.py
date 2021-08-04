dc49b99e259cbb1e0e7d4c3b835ea608ab6aa740
BraNanaK <princesamuelpks@gmail.com>
2021-08-04 21:49:32 +0000

    Create tk.py

---
+++ tk.py
@@ -0,0 +1,44 @@
+'''Tkinter cheatsheet'''
+
+# importing the tkinter library
+from tkinter import *
+
+# the root window
+root = Tk()
+
+# title of the window
+root.title("Tkinter Window")
+
+# size of the window
+root.geometry("800x600")
+
+# frame
+myFrame = Frame(root, bg="blue", height=200, width=500)
+
+# placing the frame of the window
+myFrame.place(x=150, y=50)
+
+# lable
+frameLable = Label(myFrame, text="I am dancing on a frame", font=("Sans", 18), fg="blue")
+frameLable.pack(side="right")
+
+# lable
+myLable = Label(root, text="Hello world", bg="blue", fg="white")
+myLable.place(x=10, y=50)
+
+#buttons
+myButton = Button(root, text="Click Me", height=5, width=20, bg="blue", fg="white")
+myButton.place(x=10, y=200)
+
+# entry point
+myEntry = Entry(root, font=("Sans", 14))
+myEntry.place(x=10, y=300)
+
+
+
+
+
+
+
+
+root.mainloop()
